60597f9183ff297710257094dab61f1c129581cf
Huihuo Zheng <zhenghh04@gmail.com>
2019-10-19 00:31:03 -0500

    test files

--- parallel_hdf5.py
+++ parallel_hdf5.py
@@ -5,13 +5,23 @@ comm = MPI.COMM_WORLD
 print("I am rank %s of %s"%(comm.rank, comm.size))
 if (comm.rank==0):
     print("h5py version: ", h5py.__version__)
-d1 = 8
+d1 = 9
 d2 = 5
 f = h5py.File("parallel_file.hdf5", 'w', driver='mpio', comm = comm)
 
 dset = f.create_dataset("dset", (d1, d2), dtype='i')
 dset.attrs['Units'] = "Images per second"
-for i in range(comm.rank, d1, comm.size):
+ld = d1//comm.size
+rank=comm.rank
+res=d1%comm.size
+if comm.rank<res:
+    ld = ld+1
+    offset=ld*rank
+else:
+    offset=ld*rank + res
+
+        
+for i in range(offset,offset+ld):
     for j in range(d2):
         dset[i, j] = i*d2 + j + 1
 f.close()
