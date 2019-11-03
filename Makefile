5562f662f827a574c53d867fed08dc07a449d32b
Huihuo Zheng <zhenghh04@gmail.com>
2019-11-03 16:11:45 -0600

    allow to change dimension

--- Makefile
+++ Makefile
@@ -2,16 +2,15 @@
 #!/bin/sh
 CC=mpicc
 CXX=mpicxx
-CFLAGS=-O3
-LIBS=-L$(HDF5_ROOT)/lib -lhdf5 -lmpitrace_mpich
+CFLAGS=-O3 -DDEBUG
+LIBS=-Wl,-rpath,$(HDF5_ROOT)/lib -L$(HDF5_ROOT)/lib -lhdf5 
+
+parallel_hdf5:
+	$(CXX) $(CFLAGS) -o parallel_hdf5.x parallel_hdf5.cpp $(LIBS)
 
 dset:
 	$(CXX) $(CFLAGS) -o create_dset.x create_dset.cpp $(LIBS)
 	./create_dset.x
 	h5dump ./file.h5 
-parallel_hdf5:
-	$(CXX) $(CFLAGS) -o parallel_hdf5.x parallel_hdf5.cpp $(LIBS)
-	mpirun -np 4 ./parallel_hdf5.x
-	h5dump ./parallel_file.h5 
 clean:
 	rm -rf *.x *.h5 *.hdf5
--- parallel_hdf5.cpp
+++ parallel_hdf5.cpp
@@ -1,10 +1,28 @@
 #include "hdf5.h"
 #include <iostream>
 #include "mpi.h"
+#include "stdlib.h"
+#include "stdio.h"
+#include <sys/time.h>
+#include <string.h>
 using namespace std; 
 int main(int argc, char **argv) {
   // Assuming that the dataset is a two dimensional array of 8x5 dimension;
-  hsize_t gdims[2] = {9, 5};
+  int d1 = 9; 
+  int d2 = 5; 
+  int niter = 10; 
+  for(int i=1; i<argc; i++) {
+    if (strcmp(argv[i], "--dim")==0) {
+      d1 = int(atoi(argv[i+1])); 
+      d2 = int(atoi(argv[i+2])); 
+      i+=3; 
+    } else if (strcmp(argv[i], "--niter")==0) {
+      niter = int(atoi(argv[i+1])); 
+      i+=2; 
+    }
+  }
+  hsize_t gdims[2] = {d1, d2};
+  
   MPI_Comm comm = MPI_COMM_WORLD;
   MPI_Info info = MPI_INFO_NULL;
   int rank, nproc; 
@@ -26,12 +44,21 @@ int main(int argc, char **argv) {
   hid_t plist_id = H5Pcreate(H5P_FILE_ACCESS);
   H5Pset_fapl_mpio(plist_id, comm, info);
   // Create file
+
+
+
   hid_t file_id = H5Fcreate("parallel_file.h5", H5F_ACC_TRUNC, H5P_DEFAULT, plist_id);
   H5Pclose(plist_id);
+#ifdef DEBUG
+  if (rank==0) cout << "Created file: " << endl; 
+#endif
   // create dataspace
   hid_t filespace = H5Screate_simple(2, gdims, NULL);
   hid_t dset_id = H5Dcreate(file_id, "dset", H5T_NATIVE_INT, filespace, H5P_DEFAULT,
 			    H5P_DEFAULT, H5P_DEFAULT);
+#ifdef DEBUG
+  if (rank==0) cout << "Created dataspace: " << endl; 
+#endif
 
   // define local data
   int* data = new int[ldims[0]*ldims[1]];
@@ -40,16 +67,23 @@ int main(int argc, char **argv) {
 
   // set up dataset access property list 
   plist_id = H5Pcreate(H5P_DATASET_XFER);
+
+
   H5Pset_dxpl_mpio(plist_id, H5FD_MPIO_COLLECTIVE);
   // define local memory space
   hid_t memspace = H5Screate_simple(2, ldims, NULL);
   H5Sselect_hyperslab(filespace, H5S_SELECT_SET, offset, NULL, ldims, NULL);
   // write dataset;
-  hid_t status = H5Dwrite(dset_id, H5T_NATIVE_INT, memspace, filespace, plist_id, data);
+#ifdef DEBUG
+  if (rank==0) cout << "H5Dwrite ... " << endl; 
+#endif
+  double t0 = MPI_Wtime(); 
+  for (int i=0; i<niter; i++)
+    hid_t status = H5Dwrite(dset_id, H5T_NATIVE_INT, memspace, filespace, plist_id, data);
+  double t1 = MPI_Wtime(); 
+  if (rank==0) cout <<"time: " << t1 - t0 << endl; 
   H5Pclose(plist_id);
-  
   delete [] data;
-  
   H5Dclose(dset_id);
   H5Sclose(filespace);
   H5Sclose(memspace);
